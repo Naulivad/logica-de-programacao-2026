7476cf51a0be9bdb4edf6727833eaab3490b0b93
LUAN DAVI DA CRUZ MACHADO <luan_d_machado@estudante.sesisenai.org.br>
2026-06-09 21:53:54 -0300

    exercicios

--- estrucondi/src/exercicios.js
+++ estrucondi/src/exercicios.js
@@ -1,15 +1,15 @@
 import leia from "readline-sync";
 
-var senha = leia.question("Qual sua senha? ")
+// var senha = leia.question("Qual sua senha? ")
 
-console.log ("Qual sua senha? ")
+// console.log ("Qual sua senha? ")
 
-switch (senha.toUpperCase) {
-case senha :
-    console.log("PERMITIDO")
-default:
-    console.log("INVALIDA")
-}
+// switch (senha.toUpperCase) {
+// case senha :
+//     console.log("PERMITIDO")
+// default:
+//     console.log("INVALIDA")
+// }
 
 // var senha2 = leia.question("DIGITE SUA SENHA: ")
 // var senhaP = "1234"
@@ -19,3 +19,4 @@ default:
 // }else {
 //     console.log("ACESSO NEGADO")
 // }
+
