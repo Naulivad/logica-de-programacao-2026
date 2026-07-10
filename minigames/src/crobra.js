1fbbcbed47080a402b86d3b8f6325ca964e65ebd
LUAN DAVI DA CRUZ MACHADO <luan_d_machado@estudante.sesisenai.org.br>
2026-07-09 21:34:30 -0300

    começo do jogo da croba

---
+++ minigames/src/crobra.js
@@ -0,0 +1,47 @@
+const PAREDE = "⬜";
+const VAZIO = "⬛";
+const CABECA = "🟢";
+const CORPO = "🟩";
+const COMIDA = "👽";
+
+// PLANO CARTESIANO
+
+const ALTURA = 15; // Y
+const LARGURA = 30; // X
+
+var crobraX = [10];
+var crobraY = [7];
+
+var comidaX = Math.floor(Math.random() * LARGURA);
+var comidaY = Math.floor(Math.random() * ALTURA);
+
+var pontos = 0; // ACUMULAR PONTOS KKJ
+var direcao = "d" // wasd pra andar kkj
+var gameOver = false;
+
+// FERRAMENTA DE DESENHO 
+function desenhar() {
+    var tela = "";
+    tela += "---- JOGO DA CROBRA ----\n "
+    tela += "W A S D PARA MOVER | Q === SAIR\n "
+    tela += "PONTOS " + pontos + "\n\n";
+
+    for (var y = - 1; y <= ALTURA; y++) {
+        var linha = " ";
+        for (var x = - 1; x <= LARGURA; x++) {
+            if (x === -1 || x === LARGURA || y === -1 || y === ALTURA) {
+                linha += PAREDE
+            } else if (x === comidaX && y === comidaY) {
+                linha += COMIDA;
+            } else {
+                linha += VAZIO;
+            }
+        }
+        tela += linha + "\n" // \N = PULAR LINHA!!! 
+    }
+
+    process.stdout.write("\x1b[H" + tela);
+
+}
+
+desenhar();
--- minigames/src/desafio.js
+++ minigames/src/desafio.js
@@ -1,74 +1,74 @@
 import leia from "readline-sync"
 
 
-// console.log("-------------desafio dos fios-------------")
+console.log("-------------desafio dos fios-------------")
 
-// var fioal = Math.floor(Math.random() * 4) 
+var fioal = Math.floor(Math.random() * 4) 
 
-// var tentativas = 5
+var tentativas = 5
 
-// do{
+do{
    
-//     var escolha = leia.keyInSelect(
-//         ["branco"," verde", "azul", "vermelho"], 
-//         "escolha uma cor "
-//     )
-//     if(escolha === fioal){
-//         console.log("voce ganhou o jogo era o fio  "+ (fioal +1))
-//         break
-//     }
-//     tentativas--
-//     console.log ("fio errado tente de novo")
-//     console.log("tentativas restantes " + tentativas)
+    var escolha = leia.keyInSelect(
+        ["branco"," verde", "azul", "vermelho"], 
+        "escolha uma cor "
+    )
+    if(escolha === fioal){
+        console.log("voce ganhou o jogo era o fio  "+ (fioal +1))
+        break
+    }
+    tentativas--
+    console.log ("fio errado tente de novo")
+    console.log("tentativas restantes " + tentativas)
   
 
-// }while(tentativas > 0)
-//     if(tentativas === 0){
-//         console.log("voce explodiu seu pais ")
-//     }
+}while(tentativas > 0)
+    if(tentativas === 0){
+        console.log("voce explodiu seu pais ")
+    }
 
-//professor
+professor
 
-// console.log("=== DESARME A BOMBA ===")
-// console.log("UMA BOMBA FOI IMPLANTADA")
-// console.log("VOCE PRECISA CORTAR O FIO EM 3 TENTATIVAS")
-// console.log("=======")
+console.log("=== DESARME A BOMBA ===")
+console.log("UMA BOMBA FOI IMPLANTADA")
+console.log("VOCE PRECISA CORTAR O FIO EM 3 TENTATIVAS")
+console.log("=======")
 
-// var fios = [
-//     "vermelho", "azul", "verde", "amarelo", "laranja", "rosa"
-// ]
+var fios = [
+    "vermelho", "azul", "verde", "amarelo", "laranja", "rosa"
+]
 
-// var fiocorret = fios [Math.floor(Math.random() * fios.length)]
-// var tentativas = 3;
+var fiocorret = fios [Math.floor(Math.random() * fios.length)]
+var tentativas = 3;
 
-// var desarmou = false 
+var desarmou = false 
 
-// do {
-//     var escolha = leia.keyInSelect(fios, `escollha um fio para cortar voce tem ${tentativas} tentativas`)
-//     var fioescolhido = fios[escolha]
+do {
+    var escolha = leia.keyInSelect(fios, `escollha um fio para cortar voce tem ${tentativas} tentativas`)
+    var fioescolhido = fios[escolha]
     
-//     if (fioescolhido === fiocorret) {
-//         console.log("voce desarmou a bomba")
-//         console.log ("parabens")
+    if (fioescolhido === fiocorret) {
+        console.log("voce desarmou a bomba")
+        console.log ("parabens")
 
-//         desarmou = true
+        desarmou = true
         
-//     } else {
+    } else {
 
-//         console.log("voce cortou o fio " + fioescolhido)
-//         console.log("esse nao era o fio correto")
+        console.log("voce cortou o fio " + fioescolhido)
+        console.log("esse nao era o fio correto")
 
-//         tentativas--
+        tentativas--
 
-//         fios.splice(escolha, 1)
-//     }
+        fios.splice(escolha, 1)
+    }
 
-//     console.log ("---------------------------------")
+    console.log ("---------------------------------")
 
-// }while (tentativas > 0 && desarmou === false);
+}while (tentativas > 0 && desarmou === false);
 
 
-// if (desarmou === false) {
-//     console.log ("KABUUUUMMMMM! ")
-//     console.log ("o fio correto era " + fiocorret)
-// }
+if (desarmou === false) {
+    console.log ("KABUUUUMMMMM! ")
+    console.log ("o fio correto era " + fiocorret)
+}
