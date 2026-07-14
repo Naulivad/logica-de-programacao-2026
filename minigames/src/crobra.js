ddc0e49fb01b7c87a195bc885bf82e7f30abefe4
LUAN DAVI DA CRUZ MACHADO <luan_d_machado@estudante.sesisenai.org.br>
2026-07-13 21:44:44 -0300

    fim do jogo da croba

--- minigames/src/crobra.js
+++ minigames/src/crobra.js
@@ -1,65 +1,188 @@
+import readline from 'readline'
+
+
 const PAREDE = "⬜";
 const VAZIO = "⬛";
-const CABECA = "👽";
+const CABECA = "🟢";
 const CORPO = "🟩";
 const COMIDA = "🔴";
 
-// PLANO CARTESIANO
+const LARGURA = 60; // X
+const ALTURA = 40; // Y
 
-const ALTURA = 15; // Y
-const LARGURA = 30; // X
-
-var crobraX = [10];
-var crobraY = [7];
+var cobraX = [4, 3, 2, 1];
+var cobraY = [7, 7, 7, 7];
 
 var comidaX = Math.floor(Math.random() * LARGURA);
 var comidaY = Math.floor(Math.random() * ALTURA);
 
-var pontos = 0; // ACUMULAR PONTOS KKJ
-var direcao = "d" // wasd pra andar kkj
+var pontos = 0;
+var direcao = "d";
 var gameOver = false;
 
-// FERRAMENTA DE DESENHO 
-function desenhar() {
-    var tela = "";
-    tela += "---- JOGO DA CROBRA ----\n "
-    tela += "W A S D PARA MOVER | Q === SAIR\n "
-    tela += "PONTOS " + pontos + "\n\n";
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdout.write("\x1b[?25l");
+process.stdin.on("keypress", (str, key) => {
+    console.log(str);
+    console.log(key);
 
-    for (var y = - 1; y <= ALTURA; y++) {
-        var linha = " ";
-        for (var x = - 1; x <= LARGURA; x++) {
-            if (x === -1 || x === LARGURA || y === -1 || y === ALTURA) {
-                linha += PAREDE
-            } else if (x === comidaX && y === comidaY) {
-                linha += COMIDA;
-            } else {
-                var desenhoucroba = false;
+if(key.name === "w" && direcao !== "w"){
+    direcao = "w"
+}
 
-                for (var i = 0; i < crobraX.length; i++){
-                    if(crobraX[i] === x && crobraY[i] === y){
-                        if(i === 0) {
-                            linha += CABECA;
-                        }else{
-                            linha += CORPO;
-                            desenhoucroba = true
-                        }
-                    }
+if(key.name === "a" && direcao !== "a"){
+    direcao = "a"
+}
 
-                    if (desenhoucroba === false){
-                        linha += VAZIO
-                    }
-                    
-                }
+if(key.name === "d" && direcao !== "d"){
+    direcao = "d"
+}
 
-            }
-        }
-        tela += linha + "\n" // \N = PULAR LINHA!!! 
+if(key.name === "s" && direcao !== "s"){
+    direcao = "s"
+}
+
+   if(key.name === "q"){
+        gameOver === true;
+        process.exit();
     }
+})
 
-    process.stdout.write("\x1b[H" + tela);
+function desenhar() {
+  var tela = "";
+  tela += "=== JOGO DA COBRINHA ===\n";
+  tela += "W A S D = MOVER | Q = sair\n";
+  tela += "Pontos " + pontos + "\n\n";
+
+  for (var y = -1; y <= ALTURA; y++) {
+    var linha = "";
+
+    for (var x = -1; x <= LARGURA; x++) {
+      if (x === -1 || x === LARGURA || y === -1 || y === ALTURA) {
+        linha += PAREDE;
+      } else if (x === comidaX && y === comidaY) {
+        linha += COMIDA;
+      } else {
+        var desenhouCobrinha = false;
+
+        for (var i = 0; i < cobraX.length; i++) {
+          if (cobraX[i] === x && cobraY[i] === y) {
+            linha += (i === 0) ? CABECA : CORPO;
+            desenhouCobrinha = true;
+          }
+        }
+
+        if (desenhouCobrinha === false) {
+          linha += VAZIO
+        }
+      }
+    }
+    tela += linha + "\n"
+  }
+
+  process.stdout.write("\x1b[H" + tela);
+}
+
+function sortear(){
+var posicao = false;
+
+while(posicao === false){
+comidaX = Math.floor(Math.random() * LARGURA)
+comidaY = Math.floor(Math.random() * ALTURA)
+
+posicao = true;
+for(var i = 0; i < cobraX.length; i++){
+    if(cobraX[i] === comidaX && cobraY[i] === comidaY){
+posicao = false;
+    }
+}
 
 }
 
-desenhar();
+}
 
+function movimento(){
+var novaposicaox = cobraX[0];
+var novaposicaoy = cobraY[0];
+
+switch(direcao){
+    case "w":
+        novaposicaoy--;
+        break;
+        case "s":
+            novaposicaoy++
+            break;
+            case "a":
+                novaposicaox--;
+                break;
+                case "d":
+                    novaposicaox++;
+                    break;
+
+
+}
+
+// if(novaposicaox < 0 || novaposicaox >= LARGURA || novaposicaoy <0 || novaposicaoy >= ALTURA){
+//     gameOver = true;
+//     return;
+// }
+
+if(novaposicaox < 0){
+    novaposicaox = LARGURA -1
+}
+
+if(novaposicaox >= LARGURA ){
+    novaposicaox = 0;
+}
+
+if(novaposicaoy < 0){
+    novaposicaoy = ALTURA -1
+}
+
+if(novaposicaoy >= ALTURA ){
+    novaposicaoy = 0;
+}
+
+for(var i = 0; i < cobraX.length; i++){
+    if(novaposicaox === cobraX[i] && novaposicaoy === cobraY){
+        gameOver = true;
+        return;
+    }
+}
+
+
+
+cobraX.unshift(novaposicaox)
+cobraY.unshift(novaposicaoy)
+
+if(novaposicaox === comidaX && novaposicaoy === comidaY){
+    pontos+= 10;
+    sortear();
+}else{
+    cobraX.pop()
+cobraY.pop()
+}
+
+
+}
+
+var jogo = setInterval(() => {
+    if(gameOver === true){
+        clearInterval(jogo);
+        process.stdout.write("\xb1[?")
+        console.log("\n=== PERDEU, ABUSADO ===")
+        console.log("Pontuação final : " + pontos)
+        process.exit();
+    }
+})
+
+setInterval(() => {
+    movimento();
+    desenhar();
+
+}, 150)
+
+sortear()
+desenhar();
