fd704a62010de6df6bd7945672cf721eb4d31aeb
LUAN DAVI DA CRUZ MACHADO <luan_d_machado@estudante.sesisenai.org.br>
2026-07-20 21:45:13 -0300

    jogo das idosas hehe

---
+++ estrutura_dados/src/exercicios.js
@@ -0,0 +1,15 @@
+import leia from "readline-sync"
+
+//3
+
+var numeros = [];
+
+for(var i = 0; i < 10; i++){
+    numeros.push(leia.questionInt("INFORME O NUMERO: "))
+}
+
+for (var i = 0; i < 10; i ++){
+    if(numeros[i] % 2 === 0) {
+        con
+    }
+}
