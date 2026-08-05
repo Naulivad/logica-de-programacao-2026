58cce1a4ceee29885f85c1d20c08443ce7b8ef33
LUAN DAVI DA CRUZ MACHADO <luan_d_machado@estudante.sesisenai.org.br>
2026-08-04 21:44:56 -0300

    kfghdfkj

--- estrutura_dados copy/src/explicacaodados.js
+++ estrutura_dados copy/src/explicacaodados.js
@@ -1,86 +1,39 @@
 import leia from "readline-sync"
 
-// var listaNomes = ["LELECS", "RICK", "NICULAX"]
+var users = [];
 
-// console.log(listaNomes);
-// console.log(listaNomes[1]);
-// console.log(listaNomes[2]);
+for (var i = 0; i < 3 ; i ++){
+var nome = leia.question("DIGITE SEU NOME: ")
+var email = leia.question("DIGITE SEU EMAIL: ")
+var idade = leia.questionInt("DIGITE SUA IDADE: ")
 
-// //add o 3
-
-// var contador = 3;
-// listaNomes[contador] = "CLAUDIO"
-// console.log(listaNomes[3]);
-
-// //add NO FIM DA LISTA
-
-// listaNomes.push("RICHE")
-// console.log(listaNomes[4]);
-
-// //add no inicio da lista
-
-// listaNomes.unshift("BORA BILL")
-
-// //remover o ultimo da lista
-
-// listaNomes.pop();
-
-// //remover do inicio
-
-// listaNomes.shift();
-
-// //subistitui um deles
-
-// listaNomes[4] = "LUQI"
-// console.log(listaNomes)
-
-// //tamanho da lista
-
-// console.log(listaNomes.length)
-
-// //ultimo da lista
-
-// console.log(listaNomes[listaNomes.length - 1])
-
-// //primeiro da lista
-
-// console.log(listaNomes[0])
-
-// for(var contador = 0; contador < listaNomes.length; contador++){
-
-//         listaNomes[contador] = "NADA";
-//     }
-
-// listaNomes.forEach((item, index) => {
-//     listaNomes[index] = "NADA"
-// })
-// // penecox
-//     console.log(listaNomes)
-
-var tabu = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-]
-
-function pedirPosicao (index) {
-  var linha = leia.questionInt("DIGITE A LINHA") ;
-  var coluna = leia.questionInt("DIGITE A COLUNA") ;
-  tabu[linha][coluna] = index % 2 === 0 ? "X" : "O";
-}
-
-function jogoDaVelhaDesenhar(){
-  for (var i = 0; i < tabu[0].length; i ++) {
-    console.log(" " + tabu[i][0] + " | " + tabu [i][1] + " | " + tabu [i][2])
-    if(i < 2) {
-        console.log("-----------")
-
-    }
-}
+users.push({
+  nome,
+  idade,
+  email
+});
 
 }
 
-for (var i = 0; i < 9; i ++){
-    pedirPosicao(i)
-    jogoDaVelhaDesenhar()
+users.forEach(usuario => {
+  mostraruser(usuario)
+}) 
+
+export function mostraruser(usuario){
+  var { nome, email, idade } = usuario
+  console.log(" OLA " + nome + " VOCE TEM " + idade + " ANOS, E SEU EMAIL É " + email)
 }
+
+//!!!!!! usuario[2].email = email da segunda lista !!!!!!!
+
+// var user = {
+//   nome: "lauan",
+//   email: "lulugaymepleis@gmail.com",
+//   senha: "12345678", 
+//   hello: () => console.log("HELLOWWWWWW")
+// }
+
+// console.log(user)
+// console.log(user.nome)
+// user.hello()
+// user.nome = luan
